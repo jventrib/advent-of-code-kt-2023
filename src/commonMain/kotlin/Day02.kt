2f8d78e07a6186115fb5935dc74707f909a1a296
jventrib <jventrib@gmail.com>
2023-12-03 21:38:11 +0100

    Day02 OK

--- src/commonMain/kotlin/Day02.kt
+++ src/commonMain/kotlin/Day02.kt
@@ -1,9 +1,33 @@
 val day02 = day(2) {
-    part1(expectedExampleOutput = 0, expectedOutput = 0) {
-        0
+    part1(expectedExampleOutput = 8, expectedOutput = 2486) {
+        input.mapIndexed { i, l ->
+            val map = l.substringAfter(':').split(';').all { s ->
+                s.split(',').map { c ->
+                    c.trim().split(' ').run { Cube.valueOf(last()) to first().toInt() }
+                }.all {
+                    it.second <= it.first.max
+                }
+            }
+            if (map) i + 1 else 0
+
+        }.sum()
     }
 
-    part2(expectedExampleOutput = 0, expectedOutput = 0) {
-        0
+    part2(expectedExampleOutput = 2286, expectedOutput = 87984) {
+        input.sumOf { l ->
+            l.substringAfter(':').split(';')
+                .flatMap { s ->
+                    s.split(',').map { c ->
+                        c.trim().split(' ').run { Cube.valueOf(last()) to first().toInt() }
+                    }
+                }
+                .groupBy { it.first }
+                .mapValues { it.value.maxOf { it.second } }
+                .values.reduce { acc, i -> acc * i }
+        }
     }
+}
+
+enum class Cube(val max: Int) {
+    red(12), green(13), blue(14)
 }
