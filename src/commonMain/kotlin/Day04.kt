c61f56d5b45593d080993b7fcac3c85dec39256c
jventrib <jventrib@gmail.com>
2023-12-04 18:42:11 +0100

    Day04 OK

--- src/commonMain/kotlin/Day04.kt
+++ src/commonMain/kotlin/Day04.kt
@@ -1,9 +1,36 @@
+import kotlin.math.pow
+
 val day04 = day(4) {
-    part1(expectedExampleOutput = 0, expectedOutput = 0) {
-        0
+    val cards = input
+        .mapIndexed { i, l ->
+            l.substringAfter(':')
+                .split('|')
+                .map { it.split("\\D+".toRegex()) }
+                .map { it.filter(String::isNotBlank) }
+                .map { it.map(String::toInt) }
+                .run { Card(i + 1, first(), last()) }
+        }
+
+    part1(expectedExampleOutput = 13, expectedOutput = 23235) {
+        cards
+            .map { c -> c.winning.intersect(c.player.toSet()).size }
+            .filter { it > 0 }
+            .sumOf { 2.0.pow(it - 1).toInt() }
     }
 
-    part2(expectedExampleOutput = 0, expectedOutput = 0) {
-        0
+    part2(expectedExampleOutput = 30, expectedOutput = 5920640) {
+        val queue = ArrayDeque<Card>()
+        queue.addAll(cards)
+        var total = cards.size
+        while (queue.isNotEmpty()) {
+            val c = queue.removeFirst()
+            val matching = c.winning.intersect(c.player.toSet()).size
+            val cardList = (c.number until c.number + matching).map { cards[it] }
+            queue.addAll(cardList)
+            total += cardList.size
+        }
+        total
     }
 }
+
+private data class Card(val number: Int, val winning: List<Int>, val player: List<Int>)
